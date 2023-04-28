11ce5e7569e98be5220024b5ba7e6d26bc4883d6
Brandon Webb <brandonwebb1@outlook.com>
2023-04-28 00:11:21 -0300

    ShipMovement&CameraFollow
    Ship moves in space, camera follows while in solar system

---
+++ Astro-Buccaneers/Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float rotationSpeed = 180f; // The speed at which the ship rotates
+    private Rigidbody2D rb;
+    private Vector2 targetPosition;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        targetPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        transform.Rotate(0, 0, -horizontal * rotationSpeed * Time.deltaTime);
+
+        Vector2 movement = new Vector2(0, vertical);
+        movement = transform.TransformDirection(movement); // Apply the rotation to the movement vector
+        targetPosition += movement * speed * Time.deltaTime;
+
+        // Lerp between the current position and the target position
+        transform.position = Vector2.Lerp(transform.position, targetPosition, 0.5f);
+    }
+}
