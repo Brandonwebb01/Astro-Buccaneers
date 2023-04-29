8abaffdd347d5af6a1b0627712606972295a3607
Brandon Webb <brandonwebb1@outlook.com>
2023-04-29 20:29:08 -0300

    enemy ship ai
    made the enemy ship circle the map, will follow the player if they are close

--- Astro-Buccaneers/Assets/Scripts/CameraScripts/CameraMovement.cs
+++ Astro-Buccaneers/Assets/Scripts/CameraScripts/CameraMovement.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public Transform playerTransform;
-    public float speed = 5.0f;
+    public float speed = 10.0f;
 
     public float cameraMinX = 0.1f;
     public float cameraMaxX = 15.5f;
@@ -23,12 +22,10 @@ public class CameraMovement : MonoBehaviour
         cameraPosition.x = Mathf.Clamp(cameraPosition.x, cameraMinX, cameraMaxX);
         cameraPosition.y = Mathf.Clamp(cameraPosition.y, cameraMinY, cameraMaxY);
         transform.position = cameraPosition;
-    }
 
-    void LateUpdate()
-    {
-        Vector3 desiredPosition = new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z);
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, speed * Time.deltaTime);
-        transform.position = smoothedPosition;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(horizontalInput, verticalInput, 0) * speed * Time.deltaTime;
     }
 }
