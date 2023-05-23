05f3fe66ac158d8dab9835bb8b278f4b1e79d62b
Brandon Webb <brandonwebb1@outlook.com>
2023-05-23 02:23:07 -0300

    Shooting & AI
    Player can shoot and damage enemies, enemies move to attack player. When mothership dies, drones stop being made.

--- Astro-Buccaneers/Assets/Scripts/ShipBattleScripts/EnemySpawner.cs
+++ Astro-Buccaneers/Assets/Scripts/ShipBattleScripts/EnemySpawner.cs
@@ -6,31 +6,54 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] List<WaveConfig> waveConfigs;
     [SerializeField] float timeBetweenWaves = 0f;
+    [SerializeField] bool isLooping;
     WaveConfig currentWave;
+
+    private bool enemyShipExists = true;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnEnemyWaves());
     }
 
-    public WaveConfig GetCurrentWave() {
+    public WaveConfig GetCurrentWave()
+    {
         return currentWave;
     }
 
-    IEnumerator SpawnEnemyWaves() {
-        foreach(WaveConfig wave in waveConfigs) {
-            currentWave = wave;
-                for(int i = 0; i < currentWave.GetEnemyCount(); i++) 
-                {       
+    IEnumerator SpawnEnemyWaves()
+    {
+        do
+        {
+            foreach (WaveConfig wave in waveConfigs)
+            {
+                currentWave = wave;
+                for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+                {
+                    if (!enemyShipExists) {
+                        // Break out of nested loops and exit coroutine
+                        isLooping = false;
+                        yield break;
+                    }
 
-                    Instantiate(currentWave.GetEnemyPrefab(i), 
-                    currentWave.GetStartingWayPoint().position, 
-                    Quaternion.identity, 
+                    Instantiate(currentWave.GetEnemyPrefab(i),
+                    currentWave.GetStartingWayPoint().position,
+                    Quaternion.identity,
                     transform);
 
-                yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+                    yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+                }
+                yield return new WaitForSeconds(timeBetweenWaves);
             }
-            yield return new WaitForSeconds(timeBetweenWaves);
         }
+        while (isLooping);
     }
+
+    public void OnEnemyShipDestroyed(string enemyShipName)
+{
+    if (enemyShipName == "EnemyShip")
+    {
+        enemyShipExists = false;
+    }
+}
 }
--- Astro-Buccaneers/Assets/Scripts/ShipBattleScripts/Shoot.cs
+++ Astro-Buccaneers/Assets/Scripts/ShipBattleScripts/Shoot.cs
@@ -6,15 +6,42 @@ using UnityEngine.InputSystem;
 
 public class Shoot : MonoBehaviour
 {
-    public Transform firePoint;
-    public GameObject bulletPrefab;
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] float bulletSpeed = 10f;
+    [SerializeField] float bulletLifeTime = 2f;
+    [SerializeField] float fireRate = 0.2f;
+
+    public bool isFiring;
+
+    Coroutine firingCoroutine;
+
 
     // Update is called once per frame
     void Update()
     {
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
-        {
-            Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Fire();
+    }
+
+    void Fire() {
+        if (isFiring && firingCoroutine == null) {
+            firingCoroutine = StartCoroutine(FireContinuously());
+        } else if (!isFiring && firingCoroutine != null) {
+            StopCoroutine(firingCoroutine);
+            firingCoroutine = null;
+        }
+    }
+
+    IEnumerator FireContinuously() {
+        while(true) {
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(0, bulletSpeed);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                rb.velocity = transform.up * bulletSpeed;
+            }
+            Destroy(bullet, bulletLifeTime);
+            yield return new WaitForSeconds(fireRate);
         }
     }
 }
